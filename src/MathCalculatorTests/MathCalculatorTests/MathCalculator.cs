82bc943c4eec0ffa496719b459d30e03e6589cd7
Romario1222 <roma.hlinin@yandex.ru>
2020-10-11 13:27:19 +0300

    SUSUSU

---
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
@@ -0,0 +1,19 @@
+using System;
+namespace MathCalculatorTests 
+{
+    public class MathCalculator
+    {
+        public int Sum(int a, int b){
+            return a+b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Sum(string a, string b)
+        {
+            return Convert.ToInt32(a)+Convert.ToInt32(b);
+        }
+    }
+}
