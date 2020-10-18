8bbf661564ff4078d47c0dd51f3c35edd14e1e7a
Romario1222 <roma.hlinin@yandex.ru>
2020-10-18 12:45:01 +0300

    SMTHSHIT

---
+++ src/RockScissorsPaperTests/WinnerCheckerTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using RockScissorsPaperLib;
+
+namespace RockScissorsPaperTests
+{
+    public class WinnerCheckerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            WinnerChecker winnerChecker = new WinnerChecker();
+        }
+    }
+}
