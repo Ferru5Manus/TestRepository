77521e9cb79a6d14fdb9a5bbfc8317990fc3f58b
Костя Жихарев <kostyazhikharev@gmail.com>
2020-10-18 13:37:22 +0300

    Work commit

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -6,13 +6,30 @@ namespace RockScissorsPaperLib
     {
         public string Check(string a , string b)
         {
-            if(a!="Rock" or a!="Paper" or a!="Scissors")
+            if(a!="Rock" || a!="Paper" || a!="Scissors")
             {
                 return Exeption;
             }
-            if(b!="Rock" or b!="Paper" or b!="Scissors")
+            if(b!="Rock" || b!="Paper" || b!="Scissors")
             {
                 return Exeption;
+            }
+
+            if(a != b)
+            {
+                if((a == "Rock" && b == "Scissors")||(a == "Scissors" && b == "Rock"))
+                return "Rock";
+
+                if((a == "Rock" && b == "Paper")||(a == "Paper" && b == "Rock"))
+                return "Paper";
+
+                if((a == "Paper" && b == "Scissors")||(a == "Scissors" && b == "Paper"))
+                return "Scissors";
+               
+            }
+            else
+            {
+                return "Ничья";
             }
         }
     }
