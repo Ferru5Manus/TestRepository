46f03412f62c00a963f3e0bd637d5b9d22a65675
Romario1222 <roma.hlinin@yandex.ru>
2020-10-18 13:16:06 +0300

    Check Method

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -4,5 +4,16 @@ namespace RockScissorsPaperLib
 {
     public class WinnerChecker
     {
+        public string Check(string a , string b)
+        {
+            if(a!="Rock" or a!="Paper" or a!="Scissors")
+            {
+                return Exeption;
+            }
+            if(b!="Rock" or b!="Paper" or b!="Scissors")
+            {
+                return Exeption;
+            }
+        }
     }
 }
