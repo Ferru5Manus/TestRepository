0c37752a83c4db7569af89c65b5ea29b3372c672
Костя Жихарев <kostyazhikharev@gmail.com>
2020-10-19 09:40:29 +0300

    Working test

--- src/RockScissorsPaperTests/WinnerCheckerTests.cs
+++ src/RockScissorsPaperTests/WinnerCheckerTests.cs
@@ -9,30 +9,22 @@ namespace RockScissorsPaperTests
        [Fact]
         public void Test1()
         {
-            WinerChecker winerChecker = new WinerChecker();
+            WinnerChecker winerChecker = new WinnerChecker();
 
-            var result  = winerChecker.Check("Rock","Scissor");
+            var result  = winerChecker.Check("Rock","Scissors");
                 
            Assert.Equal("Rock",result);
         }
         [Fact]
         public void Test2()
         {
-            WinerChecker winerChecker = new WinerChecker();
+            WinnerChecker winerChecker = new WinnerChecker();
 
             var result  = winerChecker.Check("Rock","Paper");
                 
             Assert.Equal("Paper",result);
         }
 
-         [Fact]
-        public void Test3()
-        {
-            WinerChecker winerChecker = new WinerChecker();
-
-            var result  = winerChecker.Check("Rockkk","Paper");
-                
-            Assert.Equal(Exception,result);
-        }
+        
     }
 }
